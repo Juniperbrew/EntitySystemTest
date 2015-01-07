2763417b7f92e6d5f14952cfb1507f780e634432
juniperbrew <ti.rasanen@gmail.com>
2015-01-07 16:18:53 +0200

    More systems

---
+++ src/MovementSystem.java
@@ -0,0 +1,29 @@
+import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.annotations.Wire;
+import com.artemis.systems.EntityProcessingSystem;
+
+@Wire
+public class MovementSystem extends EntityProcessingSystem {
+
+    ComponentMapper<Position> pm;
+    ComponentMapper<Velocity> vm;
+
+    public MovementSystem() {
+        super(Aspect.getAspectForAll(Position.class, Velocity.class));
+    }
+
+    @Override
+    protected void process(Entity e) {
+                System.out.println("Processing movement");
+                Position position = pm.get(e);
+                Velocity velocity = vm.get(e);
+
+                if(velocity == null) {
+                    return;
+                }
+                position.x += velocity.vectorX*world.delta;
+                position.y += velocity.vectorY*world.delta;
+    }
+}
